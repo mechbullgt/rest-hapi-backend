9df46761b029e2e4173d1bbab95914dfec61aa1e
mechbullgt <xyz.maqbool@gmail.com>
2019-03-24 23:08:20 +0530

    Initial setup commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// api.js
+let Hapi = require('hapi')
+let mongoose = require('mongoose')
+let RestHapi = require('rest-hapi')
+
+async function api(){
+  try {
+    let server = Hapi.Server({
+        port: 8080 
+    })
+    
+    let config = {
+        appTitle: "sparkzOnline",
+    };
+
+    await server.register({
+      plugin: RestHapi,
+      options: {
+        mongoose,
+        config
+      }
+    })
+
+    await server.start()
+
+    console.log("Server ready", server.info)
+    
+    return server
+  } catch (err) {
+    console.log("Error starting server:", err);
+  }
+}
+
+module.exports = api()
